2ac8ad973e05d13db04072623dc7e9ccc8427ab4
naoakitakado <naoakitakado666@gmail.com>
2021-06-27 05:40:41 +0900

    up

--- core/View.php
+++ core/View.php
@@ -8,6 +8,12 @@ class views
 
     public function __construct($base_dir, $defaults = array())
     {
-        $this->base_dir = $base
+        $this->base_dir = $base_dir;
+        $this->defaults = $defaults;
+    }
+
+    public function setLayoutVar($name, $value)
+    {
+        $this->layout_variables[$name] = $value;
     }
 }
