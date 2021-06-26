cfdc9d36ff614a60410d49000caaaeff1270f328
naoakitakado <naoakitakado666@gmail.com>
2021-06-27 05:38:47 +0900

    up

---
+++ core/View.php
@@ -0,0 +1,13 @@
+<?php
+
+class views
+{
+    protected $base_dir;
+    protected $defaults;
+    protected $layout_variables = array();
+
+    public function __construct($base_dir, $defaults = array())
+    {
+        $this->base_dir = $base
+    }
+}
